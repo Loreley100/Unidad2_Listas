dd9c4457c7bb76804312a62e1fd997d6f631a6c2
Loreley100 <20233tn227@utez.edu.mx>
2024-10-29 08:13:39 -0600

    se actualizan clase nodo

--- SinglyLinkedList/src/Nodo.java
+++ SinglyLinkedList/src/Nodo.java
@@ -1,9 +1,31 @@
 public class Nodo {
-    Integer dato;//el dato
-    Nodo siguiente;//la referencia 
+    private Integer dato;//el dato
+    private Nodo siguiente;//la referencia 
+
 
     // instancia para que se inicialice solito
     public Nodo(Integer dato) {
     this.dato = dato;
+    
 }
+
+
+    public Integer getDato() {
+        return dato;
+    }
+
+
+    public void setDato(Integer dato) {
+        this.dato = dato;
+    }
+
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
 }
